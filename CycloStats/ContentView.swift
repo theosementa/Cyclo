f5ae03e2f15ce4adaccd4a97cd2337324829bf33
Theo Sementa <theosementa@gmail.com>
2024-07-08 18:59:08 +0200

    Initial Commit

---
+++ CycloStats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CycloStats
+//
+//  Created by KaayZenn on 08/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CycloStats/CycloStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycloStatsApp.swift
+//  CycloStats
+//
+//  Created by KaayZenn on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CycloStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
