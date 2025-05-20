45b34cdaf9bd3ca445dda4d195c50995f73bc1e7
Theo Sementa <theosementa@gmail.com>
2025-05-20 06:55:35 +0200

    feat: add swipe back

---
+++ CycloStats/Utilities/Extensions/UINavigationController+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UINavigationController+Extensions.swift
+//  CycloStats
+//
+//  Created by Theo Sementa on 20/05/2025.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
